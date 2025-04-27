b690e1f4a2fd98e760fa7b70c8f5f27e073bf5c8
Marcin Dekier <30952768+Dekier@users.noreply.github.com>
2025-04-27 20:45:33 +0200

    add-404

---
+++ 404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ErrorPage__main-container">
+    <h1 class="ErrorPage__text">Przepraszamy. Nie ma takiej strony</h1>
+    <NuxtLink class="ErrorPage__link" to="/"
+      >Przejdź do strony głównej</NuxtLink
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ error: { statusCode: number } }>();
+</script>
+
+<style scoped lang="scss">
+@use './404.scss' as *;
+</style>
