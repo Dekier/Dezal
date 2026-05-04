0c501e1c4732ab9ed5126296a3de065add8341c2
Marcin Dekier <marcin.dekier97@gmail.com>
2026-05-04 23:47:52 +0200

    update about

--- app/components/About-company-new.vue
+++ app/components/About-company-new.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="aboutCompany" class="AboutCompany__main-container">
     <div class="AboutCompany__center-container">
+      <img
+        loading="lazy"
+        src="/icons/subtract.svg"
+        alt="icon subtract"
+        class="AboutCompany__right-icon"
+      />
+      <img
+        loading="lazy"
+        src="/icons/subtract.svg"
+        alt="icon subtract"
+        class="AboutCompany__left-icon"
+      />
       <div class="AboutCompany__left-container">
         <img
           src="/icons/logo.svg"
@@ -17,7 +29,7 @@
       <div class="AboutCompany__right-container">
         <!-- <img src="/images/about.png" class="AboutCompany__image" alt="" /> -->
         <NuxtImg
-          src="/images/about.png"
+          src="/images/about3.webp"
           alt="Żaluzje - nowoczesne systemy osłonowe Deżal"
           width="439"
           height="660"
@@ -26,6 +38,13 @@
           format="webp"
           sizes="sm:100vw md:50vw lg:439px"
         />
+        <div class="AboutCompany__white-box">
+          <h2 class="AboutCompany__big-text">
+            2 lata <br />
+            gwarancji
+          </h2>
+          <div class="AboutCompany__small-text">przy naszym montażu</div>
+        </div>
       </div>
     </div>
   </div>
