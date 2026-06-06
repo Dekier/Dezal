79f6b0c300281bf0c18158a2416285862659073f
Marcin Dekier <marcin.dekier97@gmail.com>
2026-06-06 12:00:14 +0200

    new schemaorg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -124,20 +124,36 @@ useHead({
 
 // Oficjalny, natywny sposób generowania FAQPage dostarczany przez @nuxtjs/seo
 // Unhead zintegruje to bezbłędnie ze strukturą strony bez rzucania błędów o potentialAction
-// useSchemaOrg([
-//   defineFaqPage({
-//     mainEntity: [
-//       ...faqPorady.value.map((faq) => ({
-//         name: faq.question,
-//         acceptedAnswer: faq.answer,
-//       })),
-//       ...faqTechniczne.value.map((faq) => ({
-//         name: faq.question,
-//         acceptedAnswer: faq.answer,
-//       })),
-//     ],
-//   }),
-// ]);
+useSchemaOrg([
+  // 1. Definicja Twojego fizycznego biznesu (LocalBusiness)
+  defineLocalBusiness({
+    name: 'DEŻAL Rolety. Plisy. Żaluzje. Poznań',
+    url: 'https://dezalroletypoznan.pl/',
+    logo: 'https://dezalroletypoznan.pl/images/logo-rect-2.webp',
+    telephone: '+48603630299',
+    address: {
+      streetAddress: 'ul. Jaroczyńskiego 41',
+      addressLocality: 'Poznań',
+      postalCode: '60-692',
+      addressCountry: 'PL',
+    },
+    openingHours: ['Mo 12:00-18:00', 'Tu-Fr 09:00-17:00'],
+  }),
+
+  // 2. Definicja FAQ (FAQPage)
+  defineFaqPage({
+    mainEntity: [
+      ...faqPorady.value.map((faq) => ({
+        name: faq.question,
+        acceptedAnswer: faq.answer,
+      })),
+      ...faqTechniczne.value.map((faq) => ({
+        name: faq.question,
+        acceptedAnswer: faq.answer,
+      })),
+    ],
+  }),
+]);
 </script>
 
 <style scoped lang="scss">
